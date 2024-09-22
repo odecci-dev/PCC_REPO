e89b7bbdd2837b49d17cb20c4c9ad7b6e1b110a1
odecci-dev <dev@odecci.com>
2024-09-22 11:17:53 +0800

    new update

--- API-PCC/ApplicationModels/BuffHerdFilterByModel.cs
+++ API-PCC/ApplicationModels/BuffHerdFilterByModel.cs
@@ -6,7 +6,7 @@
         public string? HerdClassDesc { get; set; }
         public string? feedingSystemCode { get; set; }
         public string? Status { get; set; }
-        public int? Center { get; set; }
+        public string? Center { get; set; }
         public string? Userid { get; set; }
     }
 }
