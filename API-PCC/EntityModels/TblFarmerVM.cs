ba9d36e64453830cf6e79e7a0a5f0d70e1e4514e
brndnfbn <brandon.fabian@odecci.com>
2024-10-14 21:26:19 +0800

    fix farmer endpoint issue

--- API-PCC/EntityModels/TblFarmerVM.cs
+++ API-PCC/EntityModels/TblFarmerVM.cs
@@ -2,7 +2,7 @@
 
 namespace API_PCC.EntityModels
 {
-    public class TblFarmers
+    public class TblFarmerVM
     {
         public int Id { get; set; }
 
@@ -21,6 +21,10 @@ namespace API_PCC.EntityModels
         public bool Is_Manager { get; set; }
         public int FarmerClassification_Id { get; set; }
         public int FarmerAffliation_Id { get; set; }
+        public List<string> FarmerBreedTypes { get; set; }
+        public List<string> FarmerFeedingSystems { get; set; }
+        //public virtual ICollection<TblFarmerBreedType>? FarmerBreedTypes { get; set; } = new List<TblFarmerBreedType>();
+        //public virtual ICollection<FarmerFeedingSystem>? FarmerFeedingSystems { get; set; } = new List<FarmerFeedingSystem>();
         public int Created_By { get; set; }
         public DateTime Created_At { get; set; }
         public int? Updated_By { get; set; }
