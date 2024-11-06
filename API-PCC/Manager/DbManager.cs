44aa7d463fb0e15802b725c358f777d2bd88e3a4
brndnfbn <brandon.fabian@odecci.com>
2024-11-06 20:22:48 +0800

    made status 4 in user registration

--- API-PCC/Manager/DbManager.cs
+++ API-PCC/Manager/DbManager.cs
@@ -29,7 +29,7 @@ namespace API_PCC.Manager
             //cnnstr = "Data Source=EC2AMAZ-V52FJK1;Initial Catalog=PCC_DEV;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server
             //cnnstr = "Data Source=WIN-RLNNENEQU3R;Initial Catalog=PCC_SIT;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server
             //cnnstr = "Data Source=WIN-RLNNENEQU3R;Initial Catalog=PCC_DEV_ENV;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server Env
-            cnnstr = "Data Source=EC2AMAZ-V52FJK1;Initial Catalog=PCC_SIT;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server ..
+            cnnstr = "Data Source=EC2AMAZ-V52FJK1;Initial Catalog=PCC_SIT;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server ...
             //cnnstr = "Data Source=DESKTOP-9P0BJ07;Initial Catalog=PCC_SIT;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server 
             //cnnstr = "Data Source=DESKTOP-9P0BJ07;Initial Catalog=PCC_DEV;User ID=pcc-server;Password=pccdev1234!"; //Odecci Server
             //cnnstr = "Data Source=DESKTOP-0SN0AHC;Initial Catalog=PCC_DEV;User ID=pcc-server;Password=pccdev1234!"; //Odecci pc 
